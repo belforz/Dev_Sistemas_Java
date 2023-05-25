6dfb403d09c70929b572b55e66bf69fd36400736
belforz <macedobeiramar@hotmail.com>
2023-05-25 19:55:07 -0300

    finalizado, agr só falta o login

--- Estacionamento/src/psv/PersonagemDAO.java
+++ Estacionamento/src/psv/PersonagemDAO.java
@@ -18,15 +18,23 @@ public class PersonagemDAO {
         this.con = con;
     }
     
-    public String inserir(PersonagemBean carro){
-        String sql = "insert into carrobean(placa, cor, descricao, modelo) values(?,?,?,?)";
+    public String inserir(PersonagemBean personagem){
+        String sql = "INSERT INTO personagens(nome, idade, status, descendencia, sangue, numero_ninjabook, cor_olhos, cor_cabelo, tamanho_roupa, filhos, observacao, aldeia)VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
         
         try{
             PreparedStatement ps = getCon().prepareStatement(sql);
-            ps.setString(1, carro.getPlaca());
-            ps.setString(2, carro.getCor());
-            ps.setString(3, carro.getDescricao());
-            ps.setString(4, carro.getModelo());
+            ps.setString(1,personagem.getNome());
+            ps.setString(2,personagem.getIdade());
+            ps.setString(3,personagem.getStatus());
+            ps.setString(4,personagem.getDescendencia());
+            ps.setString(5,personagem.getSangue());
+            ps.setString(6,personagem.getNumero());
+            ps.setString(7,personagem.getOlhos());
+            ps.setString(8,personagem.getCabelos());
+            ps.setString(9,personagem.getTamanho());
+            ps.setString(10,personagem.getFilhos());
+            ps.setString(11,personagem.getObs());
+            ps.setString(12,personagem.getAldeia());
             
             if(ps.executeUpdate() > 0){
                 return "Inserido com sucexo.";
@@ -38,15 +46,24 @@ public class PersonagemDAO {
         }
     }
     
-    public String alterar(PersonagemBean carro){
-        String sql = "update carrobean set cor = ?,descricao = ?, modelo = ?"; 
-        sql += " where placa = ?"; 
+    public String alterar(PersonagemBean personagem){
+        String sql = "update personagens set nome= ? idade =? , status =?, descendencia = ? , sangue = ?, numero_ninjabook = ? , cor_olhos = ? , cor_cabelo = ? , tamanho_roupa= ? ,  filhos = ? ,observacao = ?, aldeia = ?)"; 
+        sql += " where id = ?"; 
         try { 
         PreparedStatement ps = getCon().prepareStatement(sql); 
-        ps.setString(1, carro.getCor()); 
-        ps.setString(2, carro.getDescricao()); 
-        ps.setString(3, carro.getModelo()); 
-        ps.setString(4, carro.getPlaca()); 
+       ps.setString(1,personagem.getNome());
+            ps.setString(2,personagem.getIdade());
+            ps.setString(3,personagem.getStatus());
+            ps.setString(4,personagem.getDescendencia());
+            ps.setString(5,personagem.getSangue());
+            ps.setString(6,personagem.getNumero());
+            ps.setString(7,personagem.getOlhos());
+            ps.setString(8,personagem.getCabelos());
+            ps.setString(9,personagem.getTamanho());
+            ps.setString(10,personagem.getFilhos());
+            ps.setString(11,personagem.getObs());
+            ps.setString(12,personagem.getAldeia());
+        ps.setLong(13, personagem.getId()); 
         
         if (ps.executeUpdate() > 0) { 
         return "Alterado com sucesso."; 
@@ -58,12 +75,12 @@ public class PersonagemDAO {
         }
     }
     
-    public String excluir(PersonagemBean carro){
-        String sql = "delete from carrobean where placa = ?";
+    public String excluir(PersonagemBean personagem){
+        String sql = "delete from personagens where id = ?";
         
         try{
             PreparedStatement ps = getCon().prepareStatement(sql);
-            ps.setString(1, carro.getPlaca());
+            ps.setLong(1, personagem.getId());
             
             if(ps.executeUpdate() > 0){
                 return "Excluído com sucexo.";
@@ -76,8 +93,8 @@ public class PersonagemDAO {
     }
     
     public List<PersonagemBean> listarTodos(){
-        String sql = "select * from carrobean";
-        List<PersonagemBean> listaCarro = new ArrayList<PersonagemBean>();
+        String sql = "select * from personagens";
+        List<PersonagemBean> listaPersonagem = new ArrayList<PersonagemBean>();
         
         try{
             PreparedStatement ps = getCon().prepareStatement(sql);
@@ -85,15 +102,23 @@ public class PersonagemDAO {
             
             if(rs != null){
                 while(rs.next()){
-                    PersonagemBean cb = new PersonagemBean();                
-                    cb.setPlaca(rs.getString(1));
-                    cb.setCor(rs.getString(2));
-                    cb.setDescricao(rs.getString(3));
-                    cb.setModelo(rs.getString(4));
-                    listaCarro.add(cb);
+                    PersonagemBean pb = new PersonagemBean(); 
+                     pb.setNome(rs.getString(1)); 
+        pb.setIdade(rs.getString(2)); 
+       pb.setStatus(rs.getString(3)); 
+        pb.setDescendencia(rs.getString(4));
+         pb.setNumero(rs.getString(5)); 
+          pb.setSangue(rs.getString(6)); 
+           pb.setOlhos(rs.getString(7)); 
+            pb.setCabelos(rs.getString(8));
+             pb.setFilhos(rs.getString(9));
+              pb.setObs(rs.getString(10)); 
+           pb.setTamanho(rs.getString(11));
+            pb.setAldeia(rs.getString(12)); 
+                    listaPersonagem.add(pb);
                 }
                 
-                return listaCarro;
+                return listaPersonagem;
             }else{
                 return null;
             }
